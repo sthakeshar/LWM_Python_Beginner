3d620d26989eeeaf854913eadfcf135e52ab7059
sthakeshar <shresthakeshar291@gmail.com>
2026-04-08 11:46:19 +0900

    LWM Day_01

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
+# code to print hello world
 print('hello')
+
+# this prints my name
+print('Name: Keshar Narayan Shrestha')
+
+# this prints my current country
+print('Country: Japan')
+
+# this prints my current city
+print('city: Tokyo')
+
+# this prints my current state
+print('state: Tokyo')
