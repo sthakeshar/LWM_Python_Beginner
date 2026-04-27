bc476f36075eb94e60113b68b6fce78b5bbf974c
sthakeshar <shresthakeshar291@gmail.com>
2026-04-27 11:16:50 +0900

    LWM day 15

---
+++ list.py
@@ -0,0 +1,24 @@
+python_students = ["Niroj", "Sachin", "Surya", "Krishna", "Hari" ]
+print(python_students)
+phone_type = ["apple", "samsung", "nokia", "LG"]
+print(phone_type)
+car_company = ["Toyota", "Honda", "Tesla", "Nissan"]
+print(car_company)
+countries_name = ["India", "Nepal", "China", "USA"]
+
+mix = ["Ram", 1.23, 1, True, False]
+print(mix)
+
+os = ["windows","android","chromeos","windows 11"]
+print(os[-2])
+print(os[-3])
+print(os[-1])
+
+print(os[:])
+print(os[1:2])
+print(os[::2])
+print(os[::-3])
+print(os[-4:-2])
+
+os.append('HarmonyOS')
+print(os)
